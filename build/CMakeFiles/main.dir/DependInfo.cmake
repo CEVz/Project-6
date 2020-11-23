026c72fc373e6d1f32b70b0d5904943d5f9975c9
Clayton Vogele <vogel507@umn.edu>
2020-11-22 22:02:47 -0600

    initial files for project 6 part 1

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csugrads/vogel507/cs1521/projects/project6/oneTeller/project6.cpp" "/home/csugrads/vogel507/cs1521/projects/project6/oneTeller/build/CMakeFiles/main.dir/project6.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
